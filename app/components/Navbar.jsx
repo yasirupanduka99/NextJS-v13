a7dcd8b95a191e31d3fc447d75731582dcd6f3a5
yasirupanduka99 <22panduka@gmail.com>
2024-10-12 21:49:30 +0530

    Styles, Fonts & Images 📷

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "./Battery-Charge.svg";
 
 export default function Navbar() {
   return (
     <nav>
       <h1>NextJS v13</h1>
+      <Image
+        src={Logo}
+        alt="NextJS v13 Logo"
+        quality={100}
+      />
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
     </nav>
