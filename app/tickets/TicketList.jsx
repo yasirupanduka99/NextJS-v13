b7be408c5ce60f14ed7187b7f7ad130c0d3e79f7
yasirupanduka99 <22panduka@gmail.com>
2024-10-13 16:20:50 +0530

    Dynamic Segments (Params) 🧨

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
-        revalidate: 0 // when we add 0 second here then nextjs alwaays request and get data from db without using cache data. if you use any time frame(Ex: 30 s) then nextjs always give firstly rendered data(using cache memory) and after every 30 second nextjs request to db and getting the data
-    }
+      revalidate: 0, // when we add 0 second here then nextjs alwaays request and get data from db without using cache data. if you use any time frame(Ex: 30 s) then nextjs always give firstly rendered data(using cache memory) and after every 30 second nextjs request to db and getting the data
+    },
   }); // fetch() returns a response object with raw data in the body.
   const data = await res.json(); // res.json() converts that raw data (if it's valid JSON) into a JavaScript object.
 
@@ -18,12 +19,14 @@ export default async function TicketList() {
     <>
       {tickets.map((ticket) => (
         <div key={ticket._id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>{" "}
-          {/* slice(0, 200): This extracts the first 200 characters from ticket.body. and show ... for there is more*/}
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>{" "}
+            {/* slice(0, 200): This extracts the first 200 characters from ticket.body. and show ... for there is more*/}
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
       {tickets.length === 0 && (
